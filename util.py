2ee57f911bb3984e0d2a929076c31f9e58909bda
Matt Gormley <mrg@cs.jhu.edu>
2010-12-07 06:56:20 +0000

    Adding experiment scripts for parallel programming
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/select_pref/trunk@112 bf801614-116f-4f24-8474-ab90d4dec98d

---
+++ util.py
@@ -0,0 +1,41 @@
+import os
+import math
+
+def get_new_path(f, prefix="temp", suffix="", dir=None):
+    if(dir != None):
+        prefix = os.path.join(dir, prefix)
+    prefix = os.path.abspath(prefix)
+    num_digits = 3
+    i = 0
+    template = prefix + "_%" + str(num_digits) + "." + str(num_digits) + "d" + suffix
+    for i in range(int(math.pow(num_digits,10))):
+        path = template % (i)
+        path = os.path.abspath(path)
+        if not os.path.exists(path):
+            return f(path)
+    raise Exception("Could not create an new file/directory with prefix %s" % (prefix))
+
+def get_new_directory(prefix="temp", suffix="", dir=None):
+    def f(path):
+        os.mkdir(path)
+        return path
+    return get_new_path(f, prefix=prefix, suffix=suffix,dir=dir)
+
+def get_new_file(prefix="temp", suffix="", dir=None):
+    def f(path):
+        return open(path ,'w'), path
+    return get_new_path(f, prefix=prefix, suffix=suffix,dir=dir)
+
+def head_sentences(in_file, out_file, num_sentences):
+    out = open(out_file, 'w')
+    _head_sentences(in_file, out, num_sentences)
+    out.close()
+
+def _head_sentences(in_file, out, num_sentences):
+    count = -1
+    for line in open(in_file, 'r'):
+        out.write(line)
+        if line == '\n':
+            count += 1
+        if count == num_sentences:
+            break
