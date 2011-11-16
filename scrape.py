484a2cab12e309b192c86c3be272b375e5e3e2c3
Matt Gormley <mrg@cs.jhu.edu>
2011-11-16 01:23:37 +0000

    Updating to allow R printout
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/select_pref/trunk@1023 bf801614-116f-4f24-8474-ab90d4dec98d

--- scrape.py
+++ scrape.py
@@ -17,40 +17,129 @@ from .google_spreadsheets import get_spreadsheet_by_title,\
 import getpass
 import gdata.spreadsheet.service
 
-class Scraper:
+class ResultsWriter:
     
-    def __init__(self, print_csv=True, write_google=False, remain_only=False):
+    def __init__(self):
+        pass
+    
+    def write_readme(self, lines):
+        pass
+    
+    def write_error(self, exp_dir):
+        pass
+            
+    def write_results(self, key_order, values_list, get_as_str):
+        pass
+    
+    def get_all_as_strs(self, key_order, values_list, get_as_str):
+        key_order = map(get_as_str, key_order)
+        values_list = [map(get_as_str, values) for values in values_list]
+        return key_order, values_list
+
+class CsvResultsWriter(ResultsWriter):
+    
+    def __init__(self):
         self.sep = ","
-        self.print_csv = print_csv
-        self.write_google = write_google
-        self.remain_only = remain_only
-        if self.write_google:
-            # Get Password
-            email = 'matthew.gormley@gmail.com'
-            print "Enter password for",email
-            password = getpass.getpass()
-            
-            # Establish connection            
-            self.gd_client = gdata.spreadsheet.service.SpreadsheetsService()
-            self.gd_client.email = email
-            self.gd_client.password = password
-            self.gd_client.source = 'exampleCo-exampleApp-1'
-            self.gd_client.ProgrammaticLogin()
+        self.quote = '"'
     
-            # Get the worksheet
-            self.skey = get_spreadsheet_by_title(self.gd_client, "Temporary Results")
-            print "Spreadsheet key:",self.skey
-            self.wksht_id = get_first_worksheet(self.gd_client, self.skey)
-            print "Worksheet id:",self.wksht_id
-            
-            # Clear the worksheet
-            clear_worksheet(self.gd_client, self.skey, self.wksht_id)
-            self.row_num = 1
-            #TODO: Use self.gd_client.UpdateWorksheet(worksheet_entry) to update the num_rows/num_cols automatically
+    def csv_to_str(self, x):
+        if x.find(self.sep) != -1:
+            x = '%s%s%s' % (self.quote, x, self.quote)
+        return x
     
+    def write_readme(self, lines):
+        lines = " ".join(lines)
+        print '"' + lines.replace("\n"," ") + '"'
+        
+    def write_error(self, exp_dir):
+        # TODO: should we post this to Google Spreadsheet?
+        print self.sep.join(map(to_str,[exp_dir,"ERROR"]))
+       
+    def write_results(self, key_order, values_list, get_as_str):
+        key_order, values_list = self.get_all_as_strs(key_order, values_list, get_as_str)
+        key_order, values_list = self.get_all_as_strs(key_order, values_list, self.csv_to_str)
+        self._write_results(key_order, values_list, get_as_str)
+        
+    def _write_results(self, key_order, values_list, get_as_str):
+        # Print exp_list
+        print self.sep.join(key_order)
+        for values in values_list:
+            print self.sep.join(values)
+        print ""
+    
+class RprojResultsWriter(CsvResultsWriter):
+        
+    def __init__(self):
+        CsvResultsWriter.__init__(self)
+        self.sep = "\t"
+        self.quote = '"'
+        self.whitespace_re = re.compile("\s+")
+    
+    def csv_to_str(self, x):
+        x = self.whitespace_re.sub("_", x)
+        if x == "":
+            x = "NA"
+        return x
+        
+    def write_readme(self, lines):
+        lines = " ".join(lines)
+        sys.stderr.write('"' + lines.replace("\n"," ") + '"' + "\n")
+        
+    def write_error(self, exp_dir):
+        sys.stderr.write(self.sep.join(map(to_str,[exp_dir,"ERROR"])) + "\n")
+           
+class GoogleResultsWriter(CsvResultsWriter):
+    
+    def __init__(self):
+        CsvResultsWriter.__init__(self)
+        # Get Password
+        email = 'matthew.gormley@gmail.com'
+        print "Enter password for",email
+        password = getpass.getpass()
+        
+        # Establish connection            
+        self.gd_client = gdata.spreadsheet.service.SpreadsheetsService()
+        self.gd_client.email = email
+        self.gd_client.password = password
+        self.gd_client.source = 'exampleCo-exampleApp-1'
+        self.gd_client.ProgrammaticLogin()
+
+        # Get the worksheet
+        self.skey = get_spreadsheet_by_title(self.gd_client, "Temporary Results")
+        print "Spreadsheet key:",self.skey
+        self.wksht_id = get_first_worksheet(self.gd_client, self.skey)
+        print "Worksheet id:",self.wksht_id
+        
+        # Clear the worksheet
+        clear_worksheet(self.gd_client, self.skey, self.wksht_id)
+        self.row_num = 1
+        #TODO: Use self.gd_client.UpdateWorksheet(worksheet_entry) to update the num_rows/num_cols automatically
+        
     def write_row(self, row):
         write_row(self.gd_client, self.skey, self.wksht_id, self.row_num, row) 
-        self.row_num += 1       
+        self.row_num += 1    
+        
+    def _write_results(self, key_order, values_list, get_as_str):
+        sys.stderr.write("Writing to Google Spreadsheet...\n")
+        # Update the worksheet
+        self.write_row(key_order)
+        for values in values_list:
+            self.write_row(values)
+        
+    def write_readme(self, lines):
+        pass
+    
+    def write_error(self, exp_dir):
+        pass
+    
+class Scraper:
+    
+    def __init__(self, print_csv=True, write_google=False, remain_only=False, print_rproj=False):
+        self.remain_only = remain_only
+        self.writers = []
+        if print_rproj: self.writers.append(RprojResultsWriter())
+        if write_google: self.writers.append(GoogleResultsWriter())
+        if print_csv or len(self.writers) == 0: self.writers.append(CsvResultsWriter())
 
     def scrape(self, top_dir):
         exp_dirs = [os.path.join(top_dir,f) for f in os.listdir(top_dir) 
@@ -60,8 +149,8 @@ class Scraper:
         readme = os.path.join(top_dir, "README")
         if os.path.exists(readme):
             lines = open(readme, 'r').readlines()
-            lines = " ".join(lines)
-            print '"' + lines.replace("\n"," ") + '"'
+            for writer in self.writers:
+                writer.write_readme(lines)
             
         # Read experiment directories
         exp_list = []
@@ -95,8 +184,8 @@ class Scraper:
                     self.scrape_exp(exp, exp_dir, stdout_file)
                 
             except Exception, e:
-                # TODO: should we post this to Google Spreadsheet?
-                print self.sep.join(map(to_str,[exp_dir,"ERROR"]))
+                for writer in self.writers:
+                    writer.write_error(exp_dir)
                 import traceback
                 sys.stderr.write(str(e) + '\n')
                 traceback.print_exc()
@@ -126,24 +215,8 @@ class Scraper:
             values_list.append(values)
         values_list = sorted(values_list)
         
-        if self.print_csv:
-            # Print exp_list
-            def csv_to_str(x):
-                x = exp_orderer._get_as_str(x)
-                if x.find(",") != -1:
-                    x = '"%s"' % x
-                return x
-            print self.sep.join(map(csv_to_str, key_order))
-            for values in values_list:
-                print self.sep.join(map(csv_to_str, values))
-            print ""
-
-        if self.write_google:
-            sys.stderr.write("Writing to Google Spreadsheet...\n")
-            # Update the worksheet
-            self.write_row(key_order)
-            for values in values_list:
-                self.write_row(map(csv_to_str, values))
+        for writer in self.writers:
+            writer.write_results(key_order, values_list, exp_orderer._get_as_str)
 
     def get_exp_params_instance(self):
         ''' OVERRIDE THIS METHOD: return an ExpParams object '''
