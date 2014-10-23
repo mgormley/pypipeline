03e095d6e302bc6e0ae52c2796ca03b21ef66abd
Matt Gormley <mrg@cs.jhu.edu>
2014-10-23 16:32:45 -0400

    Switching to willow

--- pypipeline/remote_prep.py
+++ pypipeline/remote_prep.py
@@ -9,7 +9,7 @@ from fabric.context_managers import prefix
 def init():
     env.use_ssh_config = True
     # Alternative: env.gateway = "%s:%s" % ("external.hltcoe.jhu.edu", "22")
-    env.host_string = "%s:%s" % ("test2", "22")
+    env.host_string = "%s:%s" % ("test3", "22")
     run("uname -a")
         
 def prep_project_git(name, install_cmd, check_local=True):
