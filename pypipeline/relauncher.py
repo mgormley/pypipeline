1b9cecefb9dd6e2ce977d241a0218934b27c4b40
Matt Gormley <mrg@cs.jhu.edu>
2015-04-24 09:55:08 -0400

    Making relaunch.py into a proper script

--- pypipeline/relauncher.py
+++ pypipeline/relauncher.py
@@ -92,16 +92,4 @@ class Relauncher:
         pass
 
 if __name__ == "__main__":
-    usage = "%prog "
-
-    parser = OptionParser(usage=usage)
-    parser.add_option(    '--test', action="store_true", help="Run without actually launching anything")
-    (options, args) = parser.parse_args(sys.argv)
-
-    if len(args) <= 1:
-        parser.print_help()
-        sys.exit(1)
-
-    relauncher = Relauncher(options.test)
-    for arg in args[1:]:
-        relauncher.relaunch(arg)
+    pass
